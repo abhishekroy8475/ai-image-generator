1c8acec8accfbaab992b5044e3d17c9c32ce36a0
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-13 15:05:52 +0530

    server: dalle route done

--- server/controllers/dalleControllers.js
+++ server/controllers/dalleControllers.js
@@ -1,3 +1,24 @@
+import { OpenAI } from 'openai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const openai = new OpenAI({ apiKey: process.env.API_KEY })
+
 export const generateImage = async (req, res) => {
-    
+    try {
+        const { prompt } = req.body
+
+        const response = await openai.images.generate({
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+        })
+
+        const image = response.data[0].url
+        res.status(200).json({ photo: image })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
 }
