5c404c2077eb0e53a99a37b370ca1376d1a69ce4
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-13 14:14:11 +0530

    server: mongodb setup, model done

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './config/connectDB.js'
 
 dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 8080
+const url = process.env.MONGO_URI
 
 app.get('/', (req, res) => {
     res.send('server running')
 })
 
 const startServer = async () => {
+    connectDB(url)
     app.listen(PORT, () => {
         console.log(`server running on port ${PORT}`)
     })
