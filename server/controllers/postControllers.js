ca18f38ad53a0a9748ae55e0f153cbd9b556c2f7
Abhishek Roy <abhishekroy8475@gmail.com>
2023-09-13 15:52:23 +0530

    server: createPost done

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,7 +1,29 @@
-export const createPost = async (req, res) => {
-    
-}
+import { v2 as cloudinary } from 'cloudinary'
+import Post from '../models/postModel.js'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 export const fetchPost = async (req, res) => {
     
+}
+
+export const createPost = async (req, res) => {
+    try {
+        const { name, prompt, photo } = req.body
+        const photoUrl = await cloudinary.uploader.upload(photo, {folder: "ai-image-generator"})
+    
+        const newPost = await Post.create({
+          name,
+          prompt,
+          photo: photoUrl.url
+        })
+    
+        res.status(200).json({ success: true, data: newPost })
+      } catch (err) {
+        res.status(500).json({ success: false, message: 'Unable to create a post, please try again' })
+    }
 }
